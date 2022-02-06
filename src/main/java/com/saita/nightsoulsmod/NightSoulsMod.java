53ad116c30cf49d37ff149635e7fc036be96b46d
unknown <saitaquack@gmail.com>
2022-02-06 13:32:52 +0100

    3.3 + Updated Discs names

--- src/main/java/com/saita/nightsoulsmod/NightSoulsMod.java
+++ src/main/java/com/saita/nightsoulsmod/NightSoulsMod.java
@@ -47,7 +47,7 @@ public class NightSoulsMod
 
     private void setup(final FMLCommonSetupEvent event)
     {
-    	LOGGER.info("Hello from NightSouls Mod Requiem 3.2!");
+    	LOGGER.info("Hello from NightSouls Mod Requiem 3.3!");
     	LOGGER.info("Loading Star Gatherer's powers...");
     	LOGGER.info("Loading Hellfire Emperor's reign...");
     	LOGGER.info("Loading Hallowed Paladin's might...");
